8a759c229aac9806a546902bf89b9319a2286e36
Iordan Ionut <mariusionut879@yahoo.com>
2024-04-02 15:02:43 +0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,6 @@ module.exports = {
         'inter': ['Inter', 'sans-serif'],
       },
       fontSize: {
-        sm: ['14px', '20px'],
-        base: ['16px', '24px'],
-        lg: ['20px', '28px'],
-        xl: ['32px', '20px'],
       },
       fontWeight: {},
       lineHeight: {},
